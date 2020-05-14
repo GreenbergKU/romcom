c1ecf53c1ae263f59e6d5be1f07557cc01b3dfc9
Suzie Q <suzieqwolf@gmail.com>
2020-05-14 16:01:15 -0600

    Adds functionality for getRandomBookCover().

--- src/main.js
+++ src/main.js
@@ -1,5 +1,13 @@
 // Create variables targetting the relevant DOM elements here 👇
+
+// - Every time the user clicks the Show New Random Cover button, a new random cover is created
+//     - hint: you may need to update the value of the provided `currentCover` variable
+//     - hint: use that Cover class!
+// - Every time the user clicks the Show New Random Cover button, the random cover is displayed
+//     - hint: you may need to create a function that displays information on the DOM
+
 var mainCoverSection = document.querySelector(".main-cover");
+var randomCoverButton = document.querySelector(".random-cover-button");
 
 // We've provided a few variables below
 var savedCovers = [
@@ -15,14 +23,16 @@ var randomDescriptor2 = descriptors[getRandomIndex(descriptors)];
 
 
 mainCoverSection.innerHTML =
-      `<img class="cover-image" src=${randomCover}>
-      <h2 class="cover-title">${randomTitle}</h2>
-      <h3 class="tagline">A tale of <span class="tagline-1">${randomDescriptor1}</span>
-      and <span class="tagline-2">${randomDescriptor2}</span></h3>
-      <img class="price-tag" src="./assets/price.png">
-      <img class="overlay" src="./assets/overlay.png">`
+    `<img class="cover-image" src=${randomCover}>
+    <h2 class="cover-title">${randomTitle}</h2>
+    <h3 class="tagline">A tale of <span class="tagline-1">${randomDescriptor1}</span>
+    and <span class="tagline-2">${randomDescriptor2}</span></h3>
+    <img class="price-tag" src="./assets/price.png">
+    <img class="overlay" src="./assets/overlay.png">
+    `
 
 // Add your event listeners here 👇
+randomCoverButton.addEventListener('click', getRandomBookCover);
 
 
 // Create your event handlers and other functions here 👇
@@ -32,3 +42,22 @@ mainCoverSection.innerHTML =
 function getRandomIndex(array) {
   return Math.floor(Math.random() * array.length);
 }
+
+function getRandomBookCover() {
+  var randomTitle = titles[getRandomIndex(titles)];
+  var randomCover = covers[getRandomIndex(covers)];
+  var randomDescriptor1 = descriptors[getRandomIndex(descriptors)];
+  var randomDescriptor2 = descriptors[getRandomIndex(descriptors)];
+  
+  currentCover = new Cover(randomCover, randomTitle, randomDescriptor1, randomDescriptor2);
+  
+  mainCoverSection.innerHTML = `
+    <img class="cover-image" src=${currentCover.cover}>
+    <h2 class="cover-title">${currentCover.title}</h2>
+    <h3 class="tagline">A tale of <span class="tagline-1">${currentCover.tagline1}</span>
+    and <span class="tagline-2">${currentCover.tagline2}</span></h3>
+    <img class="price-tag" src="./assets/price.png">
+    <img class="overlay" src="./assets/overlay.png">
+  `
+}
+
